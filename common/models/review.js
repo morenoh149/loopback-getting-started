3571485164bc3a895f4a36200bce0efc2ece541c
Harry Moreno <morenoh149@gmail.com>
2015-06-26 17:02:05 -0700

    added angularjs

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,8 @@
 module.exports = function(Review) {
-
+  Review.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+    req.body.data = Date.now();
+    req.body.publisherId = req.accessToken.userId;
+    next();
+  });
 };
